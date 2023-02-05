84898e84ca51ad360218381389face48a34b468e
Benjamin Keller <zhero240@gmail.com>
2023-02-05 22:52:00 +0200

    Sets global properties

--- MonkeyFinder/ViewModel/BaseViewModel.cs
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,20 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 
-public class BaseViewModel
+namespace MonkeyFinder.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
 {
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+    [ObservableProperty]
+    string title;
+
+    public bool IsNotBusy => !IsBusy;
+
+    public BaseViewModel()
+    {
+
+    }
 }
