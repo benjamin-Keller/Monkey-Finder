32cd47154a40a50f20b4a0399efaf83a16c39579
Benjamin Keller <zhero240@gmail.com>
2023-02-06 00:45:18 +0200

    Adds Monkey Service

---
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
+
+public class MonkeyService
+{
+    List<Monkey> monkeyList = new();
+    HttpClient httpClient;
+
+    public MonkeyService()
+    {
+        httpClient = new HttpClient();
+    }
+
+    public async Task<List<Monkey>> GetMonkeysAsync()
+    {
+        if (monkeyList.Count > 0)
+            return monkeyList;
+
+        var url = "https://montemagno.com/monkeys.json";
+
+        var response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+
+        return monkeyList;
+    }
+}
